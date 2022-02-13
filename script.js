98ade227a57f819c9ce7535426250b82899a52e4
Cindy Germain <cindymarguerite@gmail.com>
2022-02-13 06:29:53 -0700

    adding javascript

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const headings = document.getElementById("h1");
+const nav = document.getElementById("nav");
+const article = document.getElementById("article");
+
+
